8dddd7266d4a347f5a85e01a1658b09a214d7669
stanavdb <stanvdbroekk@gmail.com>
2025-02-12 17:43:00 +0100

    fix(active effects): fix issue causing plain strings in active effect values to throw errors

--- src/system/documents/active-effect.ts
+++ src/system/documents/active-effect.ts
@@ -4,19 +4,26 @@ export class CosmereActiveEffect<
     D extends foundry.abstract.DataModel = foundry.abstract.DataModel,
 > extends ActiveEffect<D> {
     public override apply(actor: Actor, change: ActiveEffect.EffectChangeData) {
-        // Grab the roll data from the actor
-        const data = actor.getRollData() as AnyObject;
-
-        // Treat the change value as a formula and evaluate it
-        const value = new Roll(change.value, data).evaluateSync().formula;
-
         // Update the change
         const newChange = {
             ...change,
-            value: value,
+            value: tryApplyRollData(actor, change.value),
         };
 
         // Execute the standard ActiveEffect application logic
         return super.apply(actor, newChange);
+    }
+}
+
+function tryApplyRollData(actor: Actor, value: string): string {
+    try {
+        // Grab the roll data from the actor
+        const data = actor.getRollData() as AnyObject;
+
+        // Treat the change value as a formula and evaluate it
+        value = new Roll(value, data).evaluateSync().formula;
+        return value;
+    } catch {
+        return value;
     }
 }
