f022b749978398a601f8f5101ece66eccc1e82a2
Mangó <mango.shadowplay@gmail.com>
2025-02-21 19:43:22 +0100

    Implement currency display from actor data

--- src/system/applications/actor/components/currency-list.ts
+++ src/system/applications/actor/components/currency-list.ts
@@ -4,7 +4,7 @@ import { TEMPLATES } from '@src/system/utils/templates';
 
 // Component imports
 import { HandlebarsApplicationComponent } from '@system/applications/component-system';
-import { BaseActorSheet } from '../base';
+import { BaseActorSheet, BaseActorSheetRenderContext } from '../base';
 
 export class ActorCurrencyListComponent extends HandlebarsApplicationComponent<
     ConstructorOf<BaseActorSheet>
@@ -12,11 +12,38 @@ export class ActorCurrencyListComponent extends HandlebarsApplicationComponent<
     static TEMPLATE = `systems/${SYSTEM_ID}/templates/${TEMPLATES.ACTOR_BASE_CURRENCY_LIST}`;
 
     /**
-     * NOTE: Unbound methods is the standard for defining actions
+     * NOTE: Unbound methods is the standard for defining actions and forms
      * within ApplicationV2
      */
+    static ACTIONS = foundry.utils.mergeObject(
+        foundry.utils.deepClone(super.ACTIONS),
+        {},
+    );
 
-    static readonly ACTIONS = {};
+    /* --- Context --- */
+
+    public _prepareContext(
+        params: object,
+        context: BaseActorSheetRenderContext,
+    ) {
+        return Promise.resolve({
+            ...context,
+
+            currencies: Object.keys(CONFIG.COSMERE.currencies).map(
+                this.prepareCurrency.bind(this),
+            ),
+        });
+    }
+
+    private prepareCurrency(currencyId: string) {
+        const currencyConfig = CONFIG.COSMERE.currencies[currencyId];
+
+        return {
+            id: currencyId,
+            config: currencyConfig,
+            total: this.application.actor.system.currency[currencyId].total,
+        };
+    }
 }
 
 // Register
