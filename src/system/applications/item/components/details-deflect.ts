241fef39417481c22a924ebcc06099d9fa1cebea
Mangó <mango.shadowplay@gmail.com>
2025-04-29 13:59:36 +0200

    Add foundational deflect section

--- src/system/applications/item/components/details-deflect.ts
+++ src/system/applications/item/components/details-deflect.ts
@@ -10,6 +10,38 @@ export class DetailsDeflectComponent extends HandlebarsApplicationComponent<
     ConstructorOf<BaseItemSheet>
 > {
     static TEMPLATE = `systems/${SYSTEM_ID}/templates/${TEMPLATES.ITEM_DETAILS_DEFLECT}`;
+
+    /* --- Actions --- */
+
+    /* --- Context --- */
+
+    public _prepareContext(params: never, context: BaseItemSheetRenderContext) {
+        return Promise.resolve({
+            ...context,
+            deflectedTypes: this.prepareDeflectedTypesData(),
+            deflectedTypesString: this.prepareDeflectedTypesString(),
+        });
+    }
+
+    private prepareDeflectedTypesData() {
+        const item = this.application.item;
+
+        return Object.entries(CONFIG.COSMERE.damageTypes).map(
+            ([id, config]) => {
+                return {
+                    id,
+                    label: config.label,
+                    active: !(config.ignoreDeflect ?? false),
+                };
+            },
+        );
+    }
+
+    private prepareDeflectedTypesString() {
+        const item = this.application.item;
+
+        return 'test';
+    }
 }
 
 // Register the component
