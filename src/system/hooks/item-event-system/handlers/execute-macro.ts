c077032e55a8b87a2c8f60f7618252b49c033092
Stan <stanvdbroekk@gmail.com>
2025-05-02 19:28:40 +0200

    feat(item event system): add execute-macro handler

---
+++ src/system/hooks/item-event-system/handlers/execute-macro.ts
@@ -0,0 +1,39 @@
+import { CosmereItem } from '@system/documents/item';
+import { CosmereActor } from '@system/documents/actor';
+import { HandlerType, Event } from '@system/types/item/events';
+
+interface ExecuteMacroHandlerConfigData {
+    uuid: string | null;
+}
+
+export function register() {
+    cosmereRPG.api.registerItemEventHandlerType({
+        type: HandlerType.ExecuteMacro,
+        label: 'Execute Macro',
+        configSchema: {
+            uuid: new foundry.data.fields.DocumentUUIDField({
+                type: 'Macro',
+                required: true,
+                initial: null,
+                label: 'Macro',
+                hint: 'The macro to execute',
+            }),
+        },
+        executor: async function (
+            this: ExecuteMacroHandlerConfigData,
+            event: Event,
+        ) {
+            if (!this.uuid) return;
+
+            // Get the macro to execute
+            const macro = (await fromUuid(this.uuid)) as Macro | null;
+            if (!macro) return;
+
+            // Execute the macro
+            await macro.execute({
+                actor: event.item.actor,
+                event,
+            });
+        },
+    });
+}
--- src/system/hooks/item-event-system/handlers/index.ts
+++ src/system/hooks/item-event-system/handlers/index.ts
@@ -1,5 +1,7 @@
 import { register as registerGrantItemsHandler } from './grant-items';
+import { register as registerExecuteMacroHandler } from './execute-macro';
 
 export function registerHandlers() {
     registerGrantItemsHandler();
+    registerExecuteMacroHandler();
 }
