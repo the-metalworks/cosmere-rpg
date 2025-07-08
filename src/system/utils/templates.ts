3e5afba4215da46239d6863cad94a910b153a19c
Stan <stanvdbroekk@gmail.com>
2025-07-08 19:20:06 +0200

    chore(talent data model): remove grant-rules

--- src/system/utils/templates.ts
+++ src/system/utils/templates.ts
@@ -131,7 +131,6 @@ export const TEMPLATES = {
         'item/ancestry/components/advancement-talent-list.hbs',
     ITEM_ANCESTRY_BONUS_TALENTS: 'item/ancestry/components/bonus-talents.hbs',
     ITEM_GOAL_REWARDS_LIST: 'item/goal/components/rewards-list.hbs',
-    ITEM_TALENT_GRANT_RULES_LIST: 'item/talent/components/grant-rules-list.hbs',
 
     // ITEM EMBEDDINGS
     ITEM_TALENT_EMBED: 'item/talent/embed.hbs',
