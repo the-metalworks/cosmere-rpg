253ab285a9c9301affa146a1cd5e89d59afb47c3
Stan <stanvdbroekk@gmail.com>
2025-11-17 16:33:43 +0100

    feat(applications): change actor and item sheets to rely on foundry native image edit action

--- src/system/applications/item/components/header.ts
+++ src/system/applications/item/components/header.ts
@@ -14,38 +14,6 @@ export class ItemHeaderComponent extends HandlebarsApplicationComponent<// typeo
 any> {
     static TEMPLATE = `systems/${SYSTEM_ID}/templates/${TEMPLATES.ITEM_BASE_HEADER}`;
 
-    /**
-     * NOTE: Unbound methods is the standard for defining actions
-     * within ApplicationV2
-     */
-    /* eslint-disable @typescript-eslint/unbound-method */
-    static ACTIONS = {
-        'edit-img': this.onEditImg,
-    };
-    /* eslint-enable @typescript-eslint/unbound-method */
-
-    /* --- Actions --- */
-    private static onEditImg(this: ItemHeaderComponent) {
-        if (!this.application.isEditable) return;
-
-        const { img: defaultImg } = CosmereItem.getDefaultArtwork(
-            this.application.item.toObject(),
-        );
-
-        void new foundry.applications.apps.FilePicker({
-            current: this.application.item.img!,
-            type: 'image',
-            redirectToRoot: [defaultImg],
-            // top: this.application.position.top + 40,
-            // left: this.application.position.left + 10,
-            callback: (path) => {
-                void this.application.item.update({
-                    img: path,
-                });
-            },
-        }).browse();
-    }
-
     /* --- Context --- */
 
     public _prepareContext(params: never, context: BaseItemSheetRenderContext) {
