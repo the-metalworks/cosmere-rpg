57efbe47eccfb1686cabccdb919090ce029758f0
dtfe <jonasaje@stud.ntnu.no>
2026-02-27 14:18:49 +0100

    Column layout for weight, scss cleanup, removed carry stat from left column

--- src/system/applications/actor/components/encumbrance-list.ts
+++ src/system/applications/actor/components/encumbrance-list.ts
@@ -2,6 +2,7 @@ import { HandlebarsApplicationComponent } from '@system/applications/component-s
 import { BaseActorSheet, BaseActorSheetRenderContext } from '../base';
 import { SYSTEM_ID } from '@src/system/constants';
 import { TEMPLATES } from '@src/system/utils/templates';
+import { ConfigureCarryCapacityDialog } from '@src/system/applications/actor/dialogs/configure-carry-capacity';
 
 export class ActorEncumbranceListComponent extends HandlebarsApplicationComponent<// typeof BaseActorSheet
 // TODO: Resolve typing issues
@@ -9,6 +10,18 @@ export class ActorEncumbranceListComponent extends HandlebarsApplicationComponen
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 any> {
     static TEMPLATE = `systems/${SYSTEM_ID}/templates/${TEMPLATES.ACTOR_BASE_ENCUMBRANCE_LIST}`;
+
+    /* eslint-disable @typescript-eslint/unbound-method */
+    static readonly ACTIONS = {
+        'configure-carry-capacity': this.onConfigureCarryCapacity,
+    };
+    /* eslint-enable @typescript-eslint/unbound-method */
+
+    private static onConfigureCarryCapacity(
+        this: ActorEncumbranceListComponent,
+    ) {
+        void ConfigureCarryCapacityDialog.show(this.application.actor);
+    }
 
     public _prepareContext(
         params: never,
@@ -18,23 +31,22 @@ any> {
 
         // Sum weights of physical items (quantity * weight.value)
         const physicalItems = actor.items.filter((i) => i.isPhysical());
-        const currentWeight = physicalItems
-            .reduce((sum, it) => {
-                const qty = Number(it.system.quantity ?? 0);
-                const w = Number(it.system.weight?.value ?? 0);
-                return sum + qty * w;
-            }, 0)
-            .toFixed(2);
+        const currentWeightNumber = physicalItems.reduce((sum, it) => {
+            const qty = Number(it.system.quantity ?? 0);
+            const w = Number(it.system.weight?.value ?? 0);
+            return sum + qty * w;
+        }, 0);
+        const currentWeight = currentWeightNumber.toFixed(2);
 
-        const carryLimit =
-            actor.system.encumbrance?.carry?.derived ??
-            actor.system.encumbrance?.carry?.value ??
-            0;
+        const carryLimit = actor.system.encumbrance?.carry?.value ?? 0;
+
+        const isOverCarry = currentWeightNumber > Number(carryLimit);
 
         return Promise.resolve({
             ...context,
             currentWeight,
             carryLimit,
+            isOverCarry,
         });
     }
 }
