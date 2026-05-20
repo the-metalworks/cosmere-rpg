ba3ff714a12d676157d424762955eb292984d411
Khunkurisu <creative.3d@live.com>
2026-05-20 02:41:19 -0500

    feat(strikes): create strike data schema and mixin
    Signed-off-by: Khunkurisu <creative.3d@live.com>

---
+++ src/system/data/item/mixins/striking.ts
@@ -0,0 +1,45 @@
+import { DieSize, Skill } from '@src/system/types/cosmere';
+
+const SCHEMA = () => ({
+    strike: new foundry.data.fields.SchemaField({
+        die: new foundry.data.fields.SchemaField({
+            size: new foundry.data.fields.StringField({
+                required: true,
+                nullable: false,
+                initial: DieSize.D4,
+                choices: Object.values(DieSize),
+            }),
+            count: new foundry.data.fields.NumberField({
+                required: true,
+                nullable: false,
+                initial: 1,
+                integer: true,
+                step: 1,
+                min: 1,
+            }),
+        }),
+        skill: new foundry.data.fields.StringField({
+            required: true,
+            nullable: false,
+            initial: Skill.LightWeapons,
+            choices: Object.values(Skill),
+        }),
+    }),
+});
+
+export type StrikingItemDataSchema = ReturnType<typeof SCHEMA>;
+
+export function StrikingItemMixin<
+    TParent extends foundry.abstract.Document.Any,
+>() {
+    return (base: typeof foundry.abstract.TypeDataModel) => {
+        return class extends base<StrikingItemDataSchema, TParent> {
+            static defineSchema() {
+                return foundry.utils.mergeObject(
+                    super.defineSchema(),
+                    SCHEMA(),
+                );
+            }
+        };
+    };
+}
