5696902dd2d99d7e9c2f44fb51bb1d4a4bd540fe
Lena Hunicke-Smith <ihunickesmith@gmail.com>
2026-07-05 13:16:45 -0600

    feat: Added functional "System Loaded" and "Stormlight Starter Rules Present" tests

--- src/system/tests/playwright/initialization.spec.ts
+++ src/system/tests/playwright/initialization.spec.ts
@@ -1,11 +1,33 @@
 import { test, expect } from './fixtures';
 
-test('Foundry accessible', async ({ page }) => {
-    await page.goto('/game');
+test('Foundry accessible', async ({ authenticatedPage: page }) => {
     await expect(page).toHaveTitle('Foundry Virtual Tabletop');
 });
 
-test('System loaded', async ({ page }) => {
-    await page.goto('/game');
-    await expect(page).toHaveTitle('Foundry Virtual Tabletop');
+test('System loaded', async ({ authenticatedPage: page }) => {
+    await page.getByRole('tab', { name: 'Game Settings' }).click();
+    await expect(
+        page.locator('#settings').getByText('Cosmere Roleplaying Game'),
+    ).toBeVisible();
+});
+
+test('Stormlight Starter Rules Present', async ({
+    authenticatedPage: page,
+}) => {
+    await page.getByRole('tab', { name: 'Compendium Packs' }).click();
+    await expect(
+        page.locator('a').filter({ hasText: 'Stormlight Starter Rules' }),
+    ).toBeVisible();
+    await page
+        .locator('a')
+        .filter({ hasText: 'Stormlight Starter Rules' })
+        .click();
+    await expect(
+        page
+            .locator('#compendium-cosmere-rpg_starter-rules')
+            .getByRole('strong'),
+    ).toBeVisible();
+    await expect(
+        page.locator('#compendium-cosmere-rpg_starter-rules'),
+    ).toContainText('Stormlight Starter Rules');
 });
