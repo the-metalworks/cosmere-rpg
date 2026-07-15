c245e719536abc16253782d40e89cc23ab31b49c
dtfe <jonasaje@stud.ntnu.no>
2026-07-16 01:53:51 +0200

    Revert "fix(system embedded documents): add special handling for adventures "
    This reverts commit 6868ba79d7fa47a085a1815d17b16ef5f6afbffe.

--- src/system/documents/system-embedded-collections/inject.ts
+++ src/system/documents/system-embedded-collections/inject.ts
@@ -1,25 +1,17 @@
 import './socket';
 
-import { SystemEmbeddedCollectionsMixin, adventureMixin } from './mixin';
+import { SystemEmbeddedCollectionsMixin } from './mixin';
 
 globalThis.Item = SystemEmbeddedCollectionsMixin(Item, {
     Item: 'items',
 });
+
 foundry.documents = {
     ...foundry.documents,
     Item: globalThis.Item,
 };
-foundry.utils.setProperty(CONFIG, 'Item.documentClass', globalThis.Item);
 
-globalThis.Adventure = adventureMixin(Adventure, {
-    items: Item,
-});
-foundry.utils.setProperty(foundry.documents, 'adventure', globalThis.Adventure);
-foundry.utils.setProperty(
-    CONFIG,
-    'Adventure.documentClass',
-    globalThis.Adventure,
-);
+foundry.utils.setProperty(CONFIG, 'Item.documentClass', globalThis.Item);
 
 declare global {
     interface ConfiguredSystemEmbeddedCollections {
