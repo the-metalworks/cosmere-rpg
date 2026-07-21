6d88bd48e79d7508c1a8a7958b97bca60dcaa22b
posadist-revolution <ihunickesmith@gmail.com>
2026-07-21 14:18:28 -0600

    feat(actor sheets): make consume section on actions show current charges (#867)
    * feat: Add current resource values to consume display
    
    * fix: de-duplicated item list resource code by moving to dedicated item-list-resource template
    
    * Revert "feat: Add current resource values to consume display"
    
    This reverts commit 8762811f6be287286101f956489e38997bd42998.
    
    * feat: Add current resource values to consume display on action lists

--- src/system/utils/templates.ts
+++ src/system/utils/templates.ts
@@ -26,6 +26,9 @@ export const TEMPLATES = {
     COMPONENT_MATCH_DOCUMENT_STEP: 'general/components/match-document-step.hbs',
     COMPONENT_MATCH_DOCUMENT_TARGET:
         'general/components/match-document-target.hbs',
+    COMPONENT_ITEM_LIST_RESOURCE: 'general/components/item-list-resource.hbs',
+    COMPONENT_MATCH_DOCUMENT_RESOURCE_TARGET:
+        'general/components/match-document-resource-target.hbs',
 
     // ACTOR BASE
     ACTOR_BASE_NAVIGATION: 'actors/parts/navigation.hbs',
