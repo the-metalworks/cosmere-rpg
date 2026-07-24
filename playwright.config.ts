3472e2b7722ddc46f421d086b4c8c2370a8f3b29
Lena Hunicke-Smith <ihunickesmith@gmail.com>
2026-07-23 19:01:47 -0600

    chore: improved secrets protection and added hardware acceleration hooks to Chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,18 +14,19 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
     testDir: './src/system/tests/playwright',
     /* Run tests in files in parallel */
-    fullyParallel: true,
+    fullyParallel: false,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
-    /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    /* For Foundry safety to start, disable all parallelism. */
+    workers: 1,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('')`. */
+        headless: true,
         baseURL: 'http://localhost:30000',
         storageState: './playwright/.auth/state.json',
         viewport: { width: 1920, height: 1080 },
@@ -42,25 +43,29 @@ export default defineConfig({
             name: 'chromium',
             use: {
                 ...devices['Desktop Chrome'],
+                launchOptions: {
+                    args: ['--use-gl=angle', '--use-angle=gl'],
+                },
                 viewport: { width: 1920, height: 1080 },
             },
         },
 
-        {
-            name: 'firefox',
-            use: {
-                ...devices['Desktop Firefox'],
-                viewport: { width: 1920, height: 1080 },
-            },
-        },
+        // Disabled for minimum test time
+        // {
+        //     name: 'firefox',
+        //     use: {
+        //         ...devices['Desktop Firefox'],
+        //         viewport: { width: 1920, height: 1080 },
+        //     },
+        // },
 
-        {
-            name: 'webkit',
-            use: {
-                ...devices['Desktop Safari'],
-                viewport: { width: 1920, height: 1080 },
-            },
-        },
+        // {
+        //     name: 'webkit',
+        //     use: {
+        //         ...devices['Desktop Safari'],
+        //         viewport: { width: 1920, height: 1080 },
+        //     },
+        // },
 
         /* Test against mobile viewports. */
         // {
