eae0206d8b91755bfde4510e9a2fe93b6b44bae4
Lena Hunicke-Smith <ihunickesmith@gmail.com>
2026-07-25 17:04:15 -0600

    feat: Add drag-and-drop ancestry/culture/path test

--- src/system/tests/playwright/character-sheet.spec.ts
+++ src/system/tests/playwright/character-sheet.spec.ts
@@ -13,6 +13,7 @@ import {
     updateActorAttribute,
     updateActorResource,
 } from './helpers/actor';
+import { html5DragAndDrop } from './helpers/drag-drop';
 
 test('Character Details Tab Interactions', async ({
     authenticatedPage: page,
@@ -61,4 +62,74 @@ test('Character Details Tab Interactions', async ({
     // Update health to more than max
     await updateActorResource(testCharacterSheet, 'Health', 100);
     await checkActorResource(testCharacterSheet, 'Health', 11, 11);
+});
+
+test('Character Ancestry/Culture/Path Drag and Drop', async ({
+    authenticatedPage: page,
+    createActor,
+}) => {
+    const testCharacter = await createActor(
+        'Test Character',
+        ActorType.Character,
+    );
+    const testCharacterSheet = await getActorSheet(page, testCharacter);
+    await checkActorResource(testCharacterSheet, 'Health', 0, 10);
+    await checkActorResource(testCharacterSheet, 'Focus', 0, 2);
+    await checkActorResource(testCharacterSheet, 'Investiture', 0, 0);
+    await checkActorAttribute(testCharacterSheet, Attribute.Strength, 0);
+    await checkActorAttribute(testCharacterSheet, Attribute.Speed, 0);
+    await checkActorAttribute(testCharacterSheet, Attribute.Intellect, 0);
+    await checkActorAttribute(testCharacterSheet, Attribute.Willpower, 0);
+    await checkActorAttribute(testCharacterSheet, Attribute.Awareness, 0);
+    await checkActorAttribute(testCharacterSheet, Attribute.Presence, 0);
+    await checkActorDefense(testCharacterSheet, AttributeGroup.Physical, 10);
+    await checkActorDefense(testCharacterSheet, AttributeGroup.Cognitive, 10);
+    await checkActorDefense(testCharacterSheet, AttributeGroup.Spiritual, 10);
+    await page.getByRole('tab', { name: 'Compendium Packs' }).click();
+    await page
+        .locator('span')
+        .filter({ hasText: 'Stormlight Starter Rules' })
+        .click();
+
+    // Add Human ancestry
+    await page.locator('a').filter({ hasText: 'Ancestries' }).click();
+    const humanAncestryElement = page.getByText('Human');
+    await html5DragAndDrop(page, humanAncestryElement, testCharacterSheet);
+    await expect(
+        testCharacterSheet.locator('app-character-ancestry'),
+    ).toContainText('Human Ancestry');
+    await page
+        .locator('#compendium-cosmere-rpg_ancestries')
+        .getByRole('button', { name: 'Close Window' })
+        .click();
+
+    // Add Herdazian Culture
+    await page.locator('a').filter({ hasText: 'Cultures' }).click();
+    const herdazianCultureElement = page.getByText('Herdazian');
+    await html5DragAndDrop(page, herdazianCultureElement, testCharacterSheet);
+    await expect(
+        testCharacterSheet.locator('app-character-culture'),
+    ).toContainText('Herdazian Culture');
+    await page
+        .locator('#compendium-cosmere-rpg_cultures')
+        .getByRole('button', { name: 'Close Window' })
+        .click();
+
+    await page.locator('a').filter({ hasText: 'Heroic Paths' }).click();
+    await page.locator('span').filter({ hasText: 'Agent' }).click();
+    const agentPathElement = page.locator('a').filter({ hasText: /^Agent$/ });
+    await html5DragAndDrop(page, agentPathElement, testCharacterSheet);
+    await expect(
+        testCharacterSheet
+            .locator('app-actor-skill')
+            .filter({ hasText: 'Insight' }),
+    ).toContainText('+ 1 Insight AWA');
+    await page.locator('a').filter({ hasText: '3' }).click();
+    await page.locator('.sheet-navigation > a:nth-child(2)').click();
+    await expect(page.locator('app-character-talents-list')).toContainText(
+        'Agent Talents',
+    );
+    await expect(page.locator('app-character-talents-list')).toContainText(
+        'Opportunist',
+    );
 });
