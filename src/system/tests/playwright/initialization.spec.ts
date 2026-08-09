6796ef009a4414bba759e80fb9d8c5f747d8f4e0
Lena Hunicke-Smith <ihunickesmith@gmail.com>
2026-08-09 17:06:44 -0600

    chore (testing): Consolidated all initialization tests to one single test

--- src/system/tests/playwright/initialization.spec.ts
+++ src/system/tests/playwright/initialization.spec.ts
@@ -1,19 +1,13 @@
 import { test, expect } from './fixtures';
 
-test('Foundry accessible', async ({ authenticatedPage: page }) => {
+test('Foundry accessible, System loaded, Stormlight Starter Rules Present', async ({
+    authenticatedPage: page,
+}) => {
     await expect(page).toHaveTitle('Foundry Virtual Tabletop');
-});
-
-test('System loaded', async ({ authenticatedPage: page }) => {
     await page.getByRole('tab', { name: 'Game Settings' }).click();
     await expect(
         page.locator('#settings').getByText('Cosmere Roleplaying Game'),
     ).toBeVisible();
-});
-
-test('Stormlight Starter Rules Present', async ({
-    authenticatedPage: page,
-}) => {
     await page.getByRole('tab', { name: 'Compendium Packs' }).click();
     await expect(
         page.locator('a').filter({ hasText: 'Stormlight Starter Rules' }),
