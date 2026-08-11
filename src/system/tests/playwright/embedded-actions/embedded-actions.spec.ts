44c1587508a1f376503caf3e061875c209562e78
posadist-revolution <ihunickesmith@gmail.com>
2026-08-11 15:12:16 -0600

    chore: add automated testing framework (#913)
    * Added Quench and Playwright to package.json
    
    * Added initial Playwright setup
    
    * Started adding local docker build tools
    
    * feat: continued work on Docker build npm script
    
    * fix: throw error if signed license file does not exist
    
    * fix: Bad "with-license" command and open Dockerworld automatically
    
    * fix: Remove dead "docker-build.js" file
    
    * feat: Added docker up and docker down to global-setup and global-teardown
    
    * fix: increase Viewport size to 1080p
    
    * feat: Added functional "System Loaded" and "Stormlight Starter Rules Present" tests
    
    * Comment out "game loaded" console log statement
    
    * fix: Fixed viewport for major browsers configuration
    
    * feat: Started adding embedded actions test spec
    
    * chore: improved secrets protection and added hardware acceleration hooks to Chromium
    
    * fix: Made playwright tests functional without hardware acceleration
    
    * feat: Started adding character sheet test spec and helpers
    
    * fix: Removed validateActor which ran in an invalid context
    
    * fix: Force id and uuid in paramsFromHook to be serialized and accessible in Playwright
    
    * feat: Made functions to check actor resource/attributes/defenses
    
    * feat: Added meaningful update actions to character-sheet.spec.ts
    
    * feat: Started adding item helper as fixture
    
    * fix: Move createActor to fixture
    
    * feat: Started adding more meaningful testing to embedded-actions test
    
    * feat: Add html5 drag and drop helper
    
    * feat: Add drag-and-drop ancestry/culture/path test
    
    * chore: Move character-sheet test spec to more specific location
    
    * fix: Point to the correct github container registry
    
    * fix(docker): generalized docker-compose and dockerfile locations and world-seed location
    
    * fix: removed unused quench-entrypoint and quench-runner scripts
    
    * fix (docker): mount the foundry_license secret at the correct location
    
    * feat(docker): add a docker:signlicense command and a warning if a signed license doesn't exist
    
    * feat(testing): Started adding character sheet actions tab tests
    
    * fix: moved all Docker related files to world-seed repo
    
    * feat: added link-world-seed script
    
    * fix (testing): removed enable-modules.mjs from system repo
    
    * feat (testing): update ActorSheetRef to a class
    
    * feat (testing): Create checkHasAction function on actor
    
    * feat (testing): Added clearNotifications function and made testing Aid functional
    
    * feat (testing): Made checkHasAction default to "-" for consume and resources
    
    * feat (testing): Added use-action test for all actions through "dodge" alphabetically
    
    * feat (testing): made itemSheetRef into a class
    
    * feat (testing): Use all actions except strike
    
    * fix (testing): Increase timeouts for tests to 1 minute and assertions to 10 seconds
    
    * fix (testing): removed unnecessary page.pause
    
    * feat (testing): Added "get most recent chat message" function and added check for chat messages after using actions
    
    * feat (testing): speed up by not forcing docker rebuild on docker:up
    
    * feat (testing): speed up by enabling HW acceleration when not running in a CI environment
    
    * feat (testing): add test to validate that weapon strike actions appear
    
    * feat (testing): character sheet strike action test now validates loaded property
    
    * chore (testing): Consolidated all initialization tests to one single test
    
    * fix (testing): renamed getLocatorForNextWindowToOpen for clarity
    
    * feat (testing): More thoroughly test resources on embedded actions
    
    * feat (testing): Added edit mode toggle on/off tests
    
    * fix (testing): validate axe is not visible when not equipped
    
    * feat (testing): Added adversary sheet actions test
    
    * fix (testing): made adversary edit mode test correctly target adversary sheet

---
+++ src/system/tests/playwright/embedded-actions/embedded-actions.spec.ts
@@ -0,0 +1,87 @@
+import { ItemType } from '@src/system/types/cosmere';
+import { test, expect } from '../fixtures';
+
+test('Create talent with embedded action', async ({
+    authenticatedPage: page,
+    createItem,
+}) => {
+    await expect(page).toHaveTitle('Foundry Virtual Tabletop');
+    const testTalent = await createItem('Test Talent', ItemType.Talent);
+    const testTalentSheet = testTalent.locator;
+    await expect(testTalentSheet.locator('.item-header')).toBeVisible();
+    await expect(testTalentSheet.locator('app-item-header'))
+        .toMatchAriaSnapshot(`
+      - img
+      - textbox: Test Talent
+      `);
+    await testTalentSheet.getByText('Actions').click();
+    const embeddedAction = await testTalent.createEmbeddedItem();
+    const embeddedActionSheet = embeddedAction.locator;
+
+    await expect(
+        testTalentSheet.getByRole('listitem').filter({ hasText: 'New Action' }),
+    ).toBeVisible();
+    await embeddedActionSheet.getByText('Details').click();
+    await expect(
+        testTalentSheet
+            .getByRole('listitem')
+            .filter({ hasText: 'New Action — —' }),
+    ).toBeVisible();
+    await embeddedActionSheet
+        .locator('select[name="system.activation.type"]')
+        .selectOption('utility');
+    await embeddedActionSheet
+        .locator('select[name="system.activation.cost.type"]')
+        .selectOption('act');
+    await embeddedActionSheet.getByRole('spinbutton').click();
+    await embeddedActionSheet.getByRole('spinbutton').fill('2');
+    await embeddedActionSheet.getByText('Details').click();
+    await expect(
+        testTalentSheet.locator('app-item-actions-list'),
+    ).toContainText('2');
+    await embeddedActionSheet
+        .locator('app-item-resource-consumption-list > .controls > a')
+        .click();
+    await expect(
+        embeddedActionSheet.locator('app-item-resource-consumption-list'),
+    ).toContainText('1 Focus from Ancestor of type Actor');
+    await expect(
+        testTalentSheet.locator('app-item-actions-list'),
+    ).toContainText('New Action 2 1 Focus —');
+
+    await page.locator('select[name="resource"]').selectOption('inv');
+    await expect(
+        embeddedActionSheet.locator('app-item-resource-consumption-list'),
+    ).toContainText('1 Investiture from Ancestor of type Actor');
+    await expect(
+        testTalentSheet.locator('app-item-actions-list'),
+    ).toContainText('New Action 2 1 Investiture —');
+
+    await page.locator('input[name="value"]').fill('1+');
+    await page.locator('#board').click({
+        position: {
+            x: 0,
+            y: 0,
+        },
+    });
+    await expect(
+        embeddedActionSheet.locator('app-item-resource-consumption-list'),
+    ).toContainText('1+ Investiture from Ancestor of type Actor');
+    await expect(
+        testTalentSheet.locator('app-item-actions-list'),
+    ).toContainText('New Action 2 1+ Investiture —');
+
+    await page.locator('input[name="value"]').fill('0-2');
+    await page.locator('#board').click({
+        position: {
+            x: 0,
+            y: 0,
+        },
+    });
+    await expect(
+        embeddedActionSheet.locator('app-item-resource-consumption-list'),
+    ).toContainText('0-2 Investiture from Ancestor of type Actor');
+    await expect(
+        testTalentSheet.locator('app-item-actions-list'),
+    ).toContainText('New Action 2 1 - 2 Investiture (Optional) —');
+});
