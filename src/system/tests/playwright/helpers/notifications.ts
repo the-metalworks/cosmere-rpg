7817d3ffae61bcacb1764d5cc1f2f6553236031a
Lena Hunicke-Smith <ihunickesmith@gmail.com>
2026-08-12 19:53:24 -0600

    chore (testing): made clearing notifications faster and disabled audio startup

--- src/system/tests/playwright/helpers/notifications.ts
+++ src/system/tests/playwright/helpers/notifications.ts
@@ -1,9 +1,5 @@
 import { Page } from '@playwright/test';
 
 export async function clearNotifications(page: Page) {
-    const notifications = page.locator('.notification');
-
-    while ((await notifications.count()) > 0) {
-        await notifications.first().click();
-    }
+    await page.evaluate(() => ui.notifications.clear());
 }
