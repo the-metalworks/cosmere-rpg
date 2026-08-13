d47871486ebd4bdf587d02dbd443b680a603a792
dtfe <113169217+dtfe@users.noreply.github.com>
2026-08-13 21:52:49 +0200

    fix(character sheets): notes collapse on rerender#627 (#914)
    * fix: fixes issue and cleans up notes section code
    
    * chore: updates relevant deprecated calls
    
    * chore: update comment
    
    * Clarify htmlField property comment
    
    Updated comment to clarify the purpose of the htmlField property.

--- src/system/applications/actor/components/notes-fields.ts
+++ src/system/applications/actor/components/notes-fields.ts
@@ -4,6 +4,34 @@ import { BaseActorSheet, BaseActorSheetRenderContext } from '../base';
 import { SYSTEM_ID } from '@src/system/constants';
 import { TEMPLATES } from '@src/system/utils/templates';
 
+type NotesSectionType = 'biography' | 'appearance' | 'notes';
+
+interface NotesSection {
+    /**
+     * The id of the section
+     */
+    id: string;
+
+    /**
+     * Nicely formatted label for the section
+     */
+    label: string;
+
+    /**
+     * Nicely formatted edit label for the section
+     */
+    editLabel: string;
+
+    /**
+     * label/id for the target html field
+     */
+    htmlField: string;
+}
+
+interface NotesSectionState {
+    expanded?: boolean;
+}
+
 export class ActorNotesFieldsComponent extends HandlebarsApplicationComponent<// typeof BaseActorSheet
 // TODO: Resolve typing issues
 // NOTE: Use any as workaround for foundry-vtt-types issues
@@ -11,11 +39,46 @@ export class ActorNotesFieldsComponent extends HandlebarsApplicationComponent<//
 any> {
     static TEMPLATE = `${TEMPLATES.DIRECTORY}${TEMPLATES.ACTOR_BASE_NOTES_FIELDS}`;
 
-    public _prepareContext(
+    protected sectionState: Record<string, NotesSectionState> = {};
+    private sectionType: string[] = ['biography', 'appearance', 'notes'];
+    private sections: NotesSection[] = [];
+
+    public async _prepareContext(
         params: unknown,
         context: BaseActorSheetRenderContext,
     ) {
-        return Promise.resolve({ ...context });
+        this.sectionType.forEach((type) => {
+            if (!(type in this.sectionState)) {
+                this.sectionState[type] = {
+                    expanded: false,
+                };
+            }
+        });
+
+        this.sections = [
+            this.prepareSection('appearance'),
+            this.prepareSection('biography'),
+            this.prepareSection('notes'),
+        ];
+
+        return Promise.resolve({
+            ...context,
+            sections: this.sections,
+            sectionState: this.sectionState,
+        });
+    }
+
+    protected prepareSection(type: NotesSectionType): NotesSection {
+        return {
+            id: type,
+            label: game.i18n.localize(
+                `COSMERE.Actor.Sheet.Details.${type.capitalize()}.Label`,
+            ),
+            editLabel: game.i18n.localize(
+                `COSMERE.Actor.Sheet.Details.${type.capitalize()}.Edit`,
+            ),
+            htmlField: `${type}Html`,
+        };
     }
 
     protected _onRender(params: AnyObject): void {
@@ -29,8 +92,18 @@ any> {
     /* --- Event handlers --- */
 
     private onClickCollapsible(event: JQuery.ClickEvent) {
-        const target = event.currentTarget as HTMLElement;
-        target?.parentElement?.classList.toggle('expanded');
+        // Get element from event
+        const target = $(event.target).closest('.html-field');
+
+        // Get the section id
+        const sectionId = target.data('field-type') as string;
+
+        // Update the section state
+        this.sectionState[sectionId].expanded =
+            !this.sectionState[sectionId].expanded;
+
+        // Set classes
+        target.toggleClass('expanded', this.sectionState[sectionId].expanded);
     }
 }
 
